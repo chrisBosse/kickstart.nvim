d4ef17e14be01643c2e23c854c7a4e01f4d6c2bd
Chris Bosse <30784811+chrisBosse@users.noreply.github.com>
2025-04-17 16:34:24 -0400

    Add git commit owner tweaks to this repository

---
+++ RUN_AFTER_CLONE-git-set-email.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo Setting git User Name and Email for this repository:
+echo
+
+git config user.name "Chris Bosse"
+git config user.email "30784811+chrisBosse@users.noreply.github.com"
+
+git config user.name
+git config user.email
